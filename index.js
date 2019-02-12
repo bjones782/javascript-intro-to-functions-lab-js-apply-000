b33daf30cbafdc017059338206b82653fb267b27
Brooke Jones <bjones782@gmail.com>
2019-02-12 03:39:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,12 @@ function sayHiToGrandma(string) {
   return "I can't hear you!"
 }
 function sayHiToGrandma(string) {
-  var upperCase = string
-  upperCase.toUpperCase() === upperCase
-  return "YES INDEED!"
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"	    
+  } else if (string === uppercase){	  
+    return "YES INDEED!"	    
+  } else if (string === "I love you, Grandma."){	  
+    return "I love you, too."	   
 }
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.")
