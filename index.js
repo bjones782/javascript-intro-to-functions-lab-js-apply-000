6bf9056a3be86c5b752555da68a9540d81008d4c
Brooke Jones <bjones782@gmail.com>
2019-02-12 03:10:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout("hello") {
-  console.log(logShout.toUpperCase())
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
